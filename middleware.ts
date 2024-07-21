89e642f78497222ee59f6672de69681ae8f11714
ondersabahat <ondersabahat@gmail.com>
2024-07-21 20:45:59 +0300

    Added webhooks

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
   }
+  publicRoutes: ["/api/webhooks/clerk"];
 });
 
 export const config = {
